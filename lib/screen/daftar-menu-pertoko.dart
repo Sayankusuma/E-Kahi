e78199393249207d8a8274dd251716250d96ffee
Adliii <130833621+Adliii@users.noreply.github.com>
2023-12-27 13:13:41 -0800

    Update daftar-menu-pertoko.dart
    (apusin komen)

--- lib/screen/daftar-menu-pertoko.dart
+++ lib/screen/daftar-menu-pertoko.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'dart:ui';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:myapp/utils.dart';
 
 class MenuTokoPage extends StatelessWidget {
   @override
